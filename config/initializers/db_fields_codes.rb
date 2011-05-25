6aa05f2dd3427bf18e358469062fa6241637f14e
Daniel <dener@maxbook.local>
2011-05-25 01:40:57 -0300

    Form fields codes stored in a centralized file

---
+++ config/initializers/db_fields_codes.rb
@@ -0,0 +1,67 @@
+# Be sure to restart your server when you modify this file.
+
+# Candidato's and Empresa's UF address information
+UF_ADDRESS_TYPES = {
+  
+  "AC - Acre"                 => '1',
+  "AL - Alagoas"              => '2',
+  "AM - Amazonas"             => '3',
+  "AP - Amapá"                => '4',
+  "BA - Bahia"                => '5',
+  "CE - Ceará"                => '6',
+  "DF - Distrito Federal"     => '7',
+  "ES - Espírito Santo"       => '8',
+  "GO - Goiás"                => '9',
+  "MA - Maranhão"             => '10',
+  "MT - Mato Grosso"          => '11',
+  "MS - Mato Grosso do Sul"   => '12',
+  "MG - Minas Gerais"         => '13',
+  "PA - Pará"                 => '14',
+  "PB - Paraíba"              => '15',
+  "PR - Paraná"               => '16',
+  "PE - Pernambuco"           => '17',
+  "PI - Piauí"                => '18',
+  "RJ - Rio de Janeiro"       => '19',
+  "RN - Rio Grande do Norte"  => '20',
+  "RO - Rondônia"             => '21',
+  "RS - Rio Grande do Sul"    => '22',
+  "RR - Roraima"              => '23',
+  "SC - Santa Catarina"       => '24',
+  "SE - Sergipe"              => '25',
+  "SP - São Paulo"            => '26',
+  "TO - Tocantins"            => '27'
+}
+
+# Candidato's and Vaga's curso information
+CURSO_TYPES = {
+  
+  "Administração"             => '1',
+  "Arquitetura"               => '2',
+  "Ciências Contábeis"        => '3',
+  "Comunicação Social"        => '4',
+  "Direito"                   => '5',
+  "Engenharia Civil"          => '6',
+  "Engenharia de Computação"  => '5',
+  "Psicologia"                => '6',
+  "Publicidade e Propaganda"  => '7',
+  "Sistemas de Informação"    => '8'
+}
+
+FORMACAO_TYPES = {
+
+  "Ensino Fundamental"         => '1',
+  "Ensino Médio"               => '2',
+  "Ensino Superior Incompleto" => '3',
+  "Ensino Superior Completo"   => '4',
+  "Mestrado"                   => '5',
+  "Doutorado"                  => '6'
+}
+
+EXPERIENCIA_TYPES =  {
+  
+  "Sem experiência"           => '1',
+  "Até 1 ano"                 => '2',
+  "De 1 a 2 anos"             => '3',
+  "De 2 a 5 anos"             => '4',
+  "Acima de 5 anos"           => '5'
+}
